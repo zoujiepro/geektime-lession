969239733a793194d7a3d108674498997e4c9895
zoujie <10471715@qq.com>
2020-08-05 10:46:59 +0800

    beanFactory as IoC

--- thinking-in-spring/ioc-container-overview/src/main/java/com/viepub/thinking/in/spring/ioc/overview/container/AnnotationApplicationContextAsIoCContainer.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/com/viepub/thinking/in/spring/ioc/overview/container/AnnotationApplicationContextAsIoCContainer.java
@@ -1,7 +1,12 @@
 package com.viepub.thinking.in.spring.ioc.overview.container;
 
-import com.viepub.thinking.in.spring.ioc.overview.repository.UserRepository;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import com.viepub.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
 
 /**
  * @Descrption :
@@ -12,6 +17,25 @@ public class AnnotationApplicationContextAsIoCContainer {
 
     private static String beanDefinitionPath = "classpath:dependency-injection.context.xml";
 
+    @Bean
+    public User user1() {
+        User user = new User();
+        user.setId(1l);
+        user.setName("viepub");
+        return user;
+    }
+
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(AnnotationApplicationContextAsIoCContainer.class);
+        applicationContext.refresh();
+
+        getBeansOfType(applicationContext);
+
+    }
+
+    private static void getBeansOfType(BeanFactory beanFactory) {
+        Map<String, User> beansOfType = ((ListableBeanFactory) beanFactory).getBeansOfType(User.class);
+        System.out.println(beansOfType);
     }
 }
